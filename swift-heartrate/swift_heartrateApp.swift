4247e0a61bd51e94446a344145f2cf6ad47c684a
Michael Tamsil <michaeltamsil@gmail.com>
2020-09-28 11:25:48 +0700

    first commit

---
+++ swift-heartrate/swift_heartrateApp.swift
@@ -0,0 +1,17 @@
+//
+//  swift_heartrateApp.swift
+//  swift-heartrate
+//
+//  Created by michael tamsil on 28/09/20.
+//
+
+import SwiftUI
+
+@main
+struct swift_heartrateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
